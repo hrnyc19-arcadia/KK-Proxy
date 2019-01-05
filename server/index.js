0246412a5263e20b4d0137e8dda294afeed7d2e8
Kevin Kim <kevinerickim@gmail.com>
2019-01-05 12:07:03 -0500

    Proxy serves up the similar-listings component

--- server/index.js
+++ server/index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Path = require('path');
+const cors = require('cors');
+const proxy = require('http-proxy-middleware');
 
+let corsOptions = {
+  'origin': true
+}
 
 let app = express();
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "*"); next(); 
+});
 
 app.use(express.static(Path.join(__dirname, '../client')));
+
+app.use('/similar-listings', proxy({
+  target: 'ec2-3-17-11-240.us-east-2.compute.amazonaws.com'
+}))
+
 
 app.listen(3000, (err) => {
   if (err) console.log(err);
